91984c7e907cecf3015882ae98bf7fef8b37c75d
Der-Zauberer <35459249+Der-Zauberer@users.noreply.github.com>
2018-03-15 14:43:26 +0100

    Add files via upload

--- control.py
+++ control.py
@@ -6,6 +6,7 @@ init()
 
 loop = True
 hotkeymode = True
+testmode = True
 automaticmode = True
 
 engine_left_forward = 10
@@ -14,8 +15,11 @@ engine_left_backward = 9
 engine_right_backward = 7
 
 frequency = 50
-duty_cycle = 75
 stop = 0
+
+HowNear = 30.0
+ReverseTime = 0.6
+TurnTime = 0.75
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -24,6 +28,9 @@ GPIO.setup(engine_left_forward ,GPIO.OUT)
 GPIO.setup(engine_right_forward ,GPIO.OUT)
 GPIO.setup(engine_left_backward ,GPIO.OUT)
 GPIO.setup(engine_right_backward ,GPIO.OUT)
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(18, GPIO.IN)
 
 pwm_engine_left_forward = GPIO.PWM(engine_left_forward, frequency)
 pwm_engine_right_forward = GPIO.PWM(engine_right_forward, frequency)
@@ -53,31 +60,81 @@ def engineOnBackward(speed):
 	pwm_engine_left_backward.ChangeDutyCycle(speed)
 	pwm_engine_right_backward.ChangeDutyCycle(speed)
 	
-def engineOnRight():
-	pwm_engine_left_forward.ChangeDutyCycle(duty_cycle)
+def engineOnRight(speed):
+	pwm_engine_left_forward.ChangeDutyCycle(speed)
 	pwm_engine_right_forward.ChangeDutyCycle(stop)
 	pwm_engine_left_backward.ChangeDutyCycle(stop)
-	pwm_engine_right_backward.ChangeDutyCycle(duty_cycle)
+	pwm_engine_right_backward.ChangeDutyCycle(speed)
 	
-def engineOnLeft():
+def engineOnLeft(speed):
 	pwm_engine_left_forward.ChangeDutyCycle(stop)
-	pwm_engine_right_forward.ChangeDutyCycle(duty_cycle)
-	pwm_engine_left_backward.ChangeDutyCycle(duty_cycle)
+	pwm_engine_right_forward.ChangeDutyCycle(speed)
+	pwm_engine_left_backward.ChangeDutyCycle(speed)
 	pwm_engine_right_backward.ChangeDutyCycle(stop)
 	
-def engineOnTimeForward():
+def engineOnTimeForward(speed):
 	engine_time = input(Style.NORMAL + Fore.YELLOW + "Time: ")
-	engineOnForward()
+	engineOnForward(speed)
 	time.sleep(int(engine_time))
 	engineOff()
 	
-def engineOnTimeBackward():
+def engineOnTimeBackward(speed):
 	engine_time = input(Style.NORMAL + Fore.YELLOW + "Time: ")
-	engineOnBackward()
+	engineOnBackward(speed)
 	time.sleep(int(engine_time))
 	engineOff()
 	
-def hotkeyhelp():
+def Measure():
+	GPIO.output(17, True)
+	time.sleep(0.00001)
+	GPIO.output(17, False)
+	StartTime = time.time()
+	StopTime = StartTime
+	
+	while GPIO.input(18)==0:
+		StartTime = time.time()
+		StopTime = StartTime
+	
+	while GPIO.input(18)==1:
+		StopTime = time.time()
+		if StopTime-StartTime >= 0.04:
+			print("Hold on there! You're too close for me to see.")
+			StopTime = StartTime
+			break
+			
+	ElapsedTime = StopTime - StartTime
+	Distance = (ElapsedTime * 34326)/2
+	return Distance
+	
+def IsNearObstacle(localHowNear):
+	Distance = Measure()
+	print("IsNearObstacle: "+str(Distance))
+	if Distance < localHowNear:
+		return True
+	else:
+		return False
+		
+def AvoidObstacle():
+	print("Backwards")
+	engineOnBackward(60)
+	time.sleep(ReverseTime)
+	engineOff()
+	print("Right")
+	engineOnRight(50)
+	time.sleep(TurnTime)
+	engineOff()
+		
+def AvoidObstacle():
+	print("Backwards")
+	engineOnBackward(60)
+	time.sleep(ReverseTime)
+	engineOff()
+	print("Right")
+	engineOnRight(50)
+	time.sleep(TurnTime)
+	engineOff()
+	
+def hotkeymodehelp():
 	print(Style.NORMAL + Fore.YELLOW + "help        - Open this interface")
 	print("-w          - Go forward for 1 second slow")
 	print("w           - Go forward for 1 second")
@@ -92,8 +149,7 @@ def hotkeyhelp():
 	print("dd          - Go right for ~90°")
 	print("exit        - Leave the hotkeymode")
 	
-	
-def help():
+def testmodehelp():
 	print(Style.NORMAL + Fore.YELLOW + "help                   - Open this interface")
 	print("engineOnForward        - Starts the engines forward")
 	print("engineOnBackard        - Starts the engines backward")
@@ -101,6 +157,20 @@ def help():
 	print("engineOnTimeBackard    - Starts the engines backward with timelimit")
 	print("engineOff              - Stopps engines")
 	print("stop                   - Stopps engines")
+	print("exit                   - Leave the testmode")
+	
+def testmodehelp():
+	print(Style.NORMAL + Fore.YELLOW + "help                   - Open this interface")
+	print("engineOff              - Stopps engines")
+	print("stop                   - Stopps engines")
+	print("exit                   - Leave the automaticmode")
+	
+def help():
+	print(Style.NORMAL + Fore.YELLOW + "help                   - Open this interface")
+	print("stop                   - Stopps engines")
+	print("testmode               - Go in Testmode")
+	print("hotkeymode             - Go in Hotkeymode")
+	print("automaticmode          - Go in Automaticmode")
 	print("exit                   - Close programm  and cleanup GPIO")
 	print("^C                     - Close programm and cleanup GPIO")
 	
@@ -109,28 +179,43 @@ try:
 	while loop == True:
 		
 		i = input(Style.BRIGHT + Fore.YELLOW + "RaspiControl $ " + Style.RESET_ALL)
-		
-		if(i == "engineOnForward"):
-			engineOnForward(75)
-			
-		if(i == "engineOnBackward"):
-			engineOnBackward(75)
-			
-		if(i == "engineOnTimeForward"):
-			engineOnTimeForward()
-			print(Style.NORMAL + Fore.YELLOW + "Successfully!")
-			
-		if(i == "engineOnTimeBackward"):
-			engineOnTimeBackward()
-			print(Style.NORMAL + Fore.YELLOW + "Successfully!")
-			
-		if(i == "engineOff"):
-			engineOff()
-			print(Style.NORMAL + Fore.YELLOW + "Successfully!")
 			
 		if(i == "stop"):
 			engineOff()
 			print(Style.NORMAL + Fore.YELLOW + "Successfully!")
+			
+		if(i == "testmode"):
+			while testmode == True:
+			
+				ite = input(Style.BRIGHT + Fore.YELLOW + "RaspiControl - Testmode $ " + Style.RESET_ALL)
+			
+				if(ite == "exit"):
+					testmode = False
+				
+				if(ite == "help"):
+					testmodehelp()
+			
+				if(ite == "engineOnForward"):
+					engineOnForward(75)
+			
+				if(ite == "engineOnBackward"):
+					engineOnBackward(75)
+			
+				if(ite == "engineOnTimeForward"):
+					engineOnTimeForward(75)
+					print(Style.NORMAL + Fore.YELLOW + "Successfully!")
+			
+				if(ite == "engineOnTimeBackward"):
+					engineOnTimeBackward(75)
+					print(Style.NORMAL + Fore.YELLOW + "Successfully!")
+			
+				if(ite == "engineOff"):
+					engineOff()
+					print(Style.NORMAL + Fore.YELLOW + "Successfully!")
+			
+				if(ite == "stop"):
+					engineOff()
+					print(Style.NORMAL + Fore.YELLOW + "Successfully!")
 			
 		if(i == "hotkeymode"):
 			while hotkeymode == True:
@@ -141,7 +226,7 @@ try:
 					hotkeymode = False
 					
 				if(ih == "help"):
-					hotkeyhelp()
+					hotkeymodehelp()
 					
 				if(ih == "-w"):
 					engineOnForward(40)
@@ -179,22 +264,22 @@ try:
 					engineOff()
 					
 				if(ih == "a"):
-					engineOnLeft()
+					engineOnLeft(75)
 					time.sleep(0.56)
 					engineOff()
 					
 				if(ih == "aa"):
-					engineOnLeft()
+					engineOnLeft(75)
 					time.sleep(1.3)
 					engineOff()
 					
 				if(ih == "d"):
-					engineOnRight()
+					engineOnRight(75)
 					time.sleep(0.56)
 					engineOff()
 					
 				if(ih == "dd"):
-					engineOnRight()
+					engineOnRight(75)
 					time.sleep(1.3)
 					engineOff()
 					
@@ -203,8 +288,21 @@ try:
 				
 				ih = input(Style.BRIGHT + Fore.YELLOW + "RaspiControl - Automaticmode $ " + Style.RESET_ALL)
 				
+				if(ih == "obstacle"):
+					GPIO.output(17, False)
+					time.sleep(0.1)
+					while True:
+						engineOnForward(60)
+						time.sleep(0.1)
+						if IsNearObstacle(HowNear):
+							engineOff()
+							AvoidObstacle()
+				
 				if(ih == "exit"):
 					automaticmode = False
+					
+				if(ih == "help"):
+					automaticmodehelp()
 			
 		if(i == "help"):
 			help()
@@ -213,7 +311,10 @@ try:
 			GPIO.cleanup()
 			loop = False
 			
-
+		hotkeymode = True
+		testmode = True
+		automaticmode = True
+		
 except KeyboardInterrupt:
 	GPIO.cleanup()
 	print("")
